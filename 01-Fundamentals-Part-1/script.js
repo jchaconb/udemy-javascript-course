f7b8d16cd07f5e825e1094644aaee1f84490bc99
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-01-26 11:31:10 -0600

    Section 2.28: Add notes to capture key concepts

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -213,3 +213,45 @@
 // const isTired = true;
 // console.log(hasDriversLicense && hasGoodVision && isTired);
 // // --- Boolean Logic ---
+
+// // --- Switch Statement ---
+// const day = 'monday';
+
+// switch(day) {
+//   case 'monday': // day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy the weekend :D');
+//     break;
+//   default:
+//     console.log('Not a valid day');
+// }
+// // --- Switch Statement ---
+
+// // --- Statements and expressions ---
+//
+// // --- Statements and expressions ---
+
+// // --- The Conditional ---
+// const age = 23;
+
+// age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+// // --- The Conditional ---
+
+
