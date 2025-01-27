e93381e26b230f84036287a5626569b62a5632e6
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-01-27 16:58:33 -0600

    Section 3.48: Add notes to capture key concepts

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -202,3 +202,52 @@
 // console.log(jonas.age); // 46
 // console.log(jonas.getSummary());
 // // --- Object Methods ---
+
+// // --- Iteration: The for loop ---
+// for(let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+// }
+// // --- Iteration: The for loop ---
+
+// --- Looping Arrays, Breaking and Continuing ----
+// const jonasArray = [
+//   'Jonas',
+//   'Schmedtmann',
+//   2037 - 1991,
+//   'teacher',
+//   ['Michael', 'Peter', 'Steven'],
+//   true
+// ];
+
+// const types = [];
+
+// for(let i = 0; i < jonasArray.length; i++) {
+//   console.log(jonasArray[i], typeof jonasArray[i]);
+//   types.push(typeof jonasArray[i]);
+// }
+
+// console.log(types);
+
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
+
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+
+// console.log(ages);
+
+// // continue and break
+
+// console.log('--- ONLY STRINGS ---')
+// for(let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] !== 'string') continue;
+//   console.log(jonasArray[i], typeof jonasArray[i]);
+// }
+
+// console.log('--- BREAK WITH NUMBER ---')
+// for(let i = 0; i < jonasArray.length; i++) {
+//   if (typeof jonasArray[i] === 'number') break;
+//   console.log(jonasArray[i], typeof jonasArray[i]);
+// }
+// // --- Looping Arrays, Breaking and Continuing ----
