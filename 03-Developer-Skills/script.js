2a070d5dd40e8310caf995cceb7a3fa7a13c3973
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-01-28 13:56:47 -0600

    Section 5.59: Create small exercise

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,21 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// --- Exercise #1 ---
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const calcTempAmplitude = function (temps) {
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 1; i < temps.length; i++) {
+    if (temps[i] === 'error') continue;
+    if (temps[i] > max) max = temps[i];
+    if (temps[i] < min) min = temps[i];
+  }
+
+  return max - min;
+};
+
+console.log(calcTempAmplitude(temperatures));
+// -------------------
