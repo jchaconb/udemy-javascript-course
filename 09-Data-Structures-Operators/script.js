3ebf9a8afae4059fb59251813cd1d82d1474697a
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-03 12:52:28 -0600

    Section 9.114: Complete Challenge

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,258 +1,356 @@
 'use strict';
 
-const restaurantA = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  order: function (starterIndex, mainIndex) {
-    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-  },
-};
+// const restaurantA = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+//   order: function (starterIndex, mainIndex) {
+//     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+//   },
+// };
 
-// Destructuring Arrays
-const arrA = [2, 3, 4];
-// const a = arrA[0];
-// const b = arrA[1];
-// const c = arrA[2];
+// // Destructuring Arrays
+// const arrA = [2, 3, 4];
+// // const a = arrA[0];
+// // const b = arrA[1];
+// // const c = arrA[2];
 
-const [x, y, z] = arrA;
+// const [x, y, z] = arrA;
 
-console.log(x, y, z); // 2 3 4
-console.log(arrA); // [2, 3, 4]
+// console.log(x, y, z); // 2 3 4
+// console.log(arrA); // [2, 3, 4]
 
-const [first, second] = restaurantA.categories;
-console.log(first, second); // Italian Pizzeria
+// const [first, second] = restaurantA.categories;
+// console.log(first, second); // Italian Pizzeria
 
-let [main, secondary] = restaurantA.categories;
-console.log(main, secondary); // Italian Pizzeria
+// let [main, secondary] = restaurantA.categories;
+// console.log(main, secondary); // Italian Pizzeria
 
-[main, secondary] = [secondary, main];
-console.log(main, secondary); // Pizzeria Italian
+// [main, secondary] = [secondary, main];
+// console.log(main, secondary); // Pizzeria Italian
 
-const [starter, mainCourse] = restaurantA.order(2, 0);
-console.log(starter, mainCourse);
+// const [starter, mainCourse] = restaurantA.order(2, 0);
+// console.log(starter, mainCourse);
 
-// Nested Destructuting
-const nested = [2, 4, [5, 6]];
-const [i, , j] = nested;
-console.log(i, j); // (2) [5, 6]
+// // Nested Destructuting
+// const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+// console.log(i, j); // (2) [5, 6]
 
-const [q, , [w, e]] = nested;
-console.log(q, w, e); // 2 5 6
+// const [q, , [w, e]] = nested;
+// console.log(q, w, e); // 2 5 6
 
-const [m, n, l] = [8, 9];
-console.log(m, n, l); // 8 9 undefined
+// const [m, n, l] = [8, 9];
+// console.log(m, n, l); // 8 9 undefined
 
-const [k = 1, h = 1, g = 1] = [8, 9];
-console.log(k, h, g); // 8 9 1
+// const [k = 1, h = 1, g = 1] = [8, 9];
+// console.log(k, h, g); // 8 9 1
 
-// Destructuring Objects
-const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+// // Destructuring Objects
+// const restaurant = {
+//   name: 'Classico Italiano',
+//   location: 'Via Angelo Tavanti 23, Firenze, Italy',
+//   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+//   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+//   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
-    console.log(starterIndex, time, mainIndex, address);
-  },
+//   orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+//     console.log(starterIndex, time, mainIndex, address);
+//   },
 
-  orderPizza: function (mainIngredient, ...otherIngredients) {
-    console.log(mainIngredient);
-    console.log(otherIngredients);
-  },
-};
+//   orderPizza: function (mainIngredient, ...otherIngredients) {
+//     console.log(mainIngredient);
+//     console.log(otherIngredients);
+//   },
+// };
 
-const { name, openingHours, categories } = restaurant;
-console.log(name, openingHours, categories);
+// const { name, openingHours, categories } = restaurant;
+// console.log(name, openingHours, categories);
 
-const {
-  name: restaurantName,
-  openingHours: hours,
-  categories: tags,
-} = restaurant;
-console.log(name, openingHours, categories);
+// const {
+//   name: restaurantName,
+//   openingHours: hours,
+//   categories: tags,
+// } = restaurant;
+// console.log(name, openingHours, categories);
 
-const { menu = [], starterMenu: starters = [] } = restaurant;
-console.log(menu, starters);
+// const { menu = [], starterMenu: starters = [] } = restaurant;
+// console.log(menu, starters);
 
-// Mutating variables
-let a = 111;
-let b = 999;
-const obj = { a: 23, b: 7, c: 14 };
+// // Mutating variables
+// let a = 111;
+// let b = 999;
+// const obj = { a: 23, b: 7, c: 14 };
 
-// This does not work without the parenthesis
-({ a, b } = obj);
+// // This does not work without the parenthesis
+// ({ a, b } = obj);
 
-// Nested objects
-const {
-  fri: { open, close },
-} = openingHours;
-console.log(open, close); // 11 23
+// // Nested objects
+// const {
+//   fri: { open, close },
+// } = openingHours;
+// console.log(open, close); // 11 23
 
-restaurant.orderDelivery({
-  time: '22:30',
-  address: 'Via del Sole, 21',
-  mainIndex: 2,
-  starterIndex: 2,
-});
+// restaurant.orderDelivery({
+//   time: '22:30',
+//   address: 'Via del Sole, 21',
+//   mainIndex: 2,
+//   starterIndex: 2,
+// });
 
-// The Spread Operator (...)
-const arr = [6, 7, 8];
-const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
-console.log(badNewArr); // [1, 2, 6, 7, 8]
+// // The Spread Operator (...)
+// const arr = [6, 7, 8];
+// const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+// console.log(badNewArr); // [1, 2, 6, 7, 8]
 
-const newArr = [1, 2, ...arr];
-console.log(newArr); // [1, 2, 6, 7, 8]
+// const newArr = [1, 2, ...arr];
+// console.log(newArr); // [1, 2, 6, 7, 8]
 
-const newMenu = [...restaurant.mainMenu, 'Gnocci'];
-console.log(newMenu);
+// const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+// console.log(newMenu);
 
-// Copy array
-const mainMenuCopy = [...restaurant.mainMenu];
-console.log(mainMenuCopy);
+// // Copy array
+// const mainMenuCopy = [...restaurant.mainMenu];
+// console.log(mainMenuCopy);
 
-// Join 2 Arrays
-const allMenus = [...restaurant.starterMenu, ...restaurant.mainMenu];
-console.log(allMenus);
+// // Join 2 Arrays
+// const allMenus = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(allMenus);
 
-// Iterables: Arrays, maps, strings, sets. NOT Objects
-const str = 'Alberto';
-const letters = [...str, '', 'Ch.'];
-console.log(letters);
+// // Iterables: Arrays, maps, strings, sets. NOT Objects
+// const str = 'Alberto';
+// const letters = [...str, '', 'Ch.'];
+// console.log(letters);
 
-// console.log(`${...str} Ch.`); //  Unexpected token '...'
+// // console.log(`${...str} Ch.`); //  Unexpected token '...'
 
-// Objects
-const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
+// // Objects
+// const newRestaurant = { foundedIn: 1998, ...restaurant, founder: 'Guiseppe' };
 
-// Rest Pattern and Parameters
+// // Rest Pattern and Parameters
 
-// Spread, because on RIGHT side of =
-const array = [1, 2, ...[3, 4]];
-console.log(array); // [1, 2, 3, 4]
-// REST, because on LEFT side of =
-const [n1, n2, ...others] = [1, 2, 3, 4, 5];
-console.log(n1, n2, others); // 1 2 [3, 4, 5]
+// // Spread, because on RIGHT side of =
+// const array = [1, 2, ...[3, 4]];
+// console.log(array); // [1, 2, 3, 4]
+// // REST, because on LEFT side of =
+// const [n1, n2, ...others] = [1, 2, 3, 4, 5];
+// console.log(n1, n2, others); // 1 2 [3, 4, 5]
 
-const [pizza, , risotto, ...otherFood] = [
-  ...restaurant.mainMenu,
-  ...restaurant.starterMenu,
-];
-console.log(pizza, risotto, otherFood); // Pizza Risotto ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
+// const [pizza, , risotto, ...otherFood] = [
+//   ...restaurant.mainMenu,
+//   ...restaurant.starterMenu,
+// ];
+// console.log(pizza, risotto, otherFood); // Pizza Risotto ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad']
 
-// Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(sat, weekdays); // {open: 0, close: 24} {thu: {…}, fri: {…}}
+// // Objects
+// const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(sat, weekdays); // {open: 0, close: 24} {thu: {…}, fri: {…}}
 
-// 2) Functions
-const add = function (...numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
+// // 2) Functions
+// const add = function (...numbers) {
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
 
-  console.log(sum);
-};
+//   console.log(sum);
+// };
 
-add(2, 3); // [2, 3]
-add(5, 3, 7, 2); // [5, 3, 7, 2]
-add(8, 2, 5, 3, 2, 1, 4); // [8, 2, 5, 3, 7, 1, 4]
+// add(2, 3); // [2, 3]
+// add(5, 3, 7, 2); // [5, 3, 7, 2]
+// add(8, 2, 5, 3, 2, 1, 4); // [8, 2, 5, 3, 7, 1, 4]
 
-const x1 = [23, 5, 7];
-add(...x1); // 35
+// const x1 = [23, 5, 7];
+// add(...x1); // 35
 
-restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
-restaurant.orderPizza('mushrooms');
+// restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+// restaurant.orderPizza('mushrooms');
 
-// Short Circuiting (&& and ||)
+// // Short Circuiting (&& and ||)
 
-// ------ || ------
-// Use ANY data type, return ANY data type;
-console.log(3 || 'Jonas'); // 3
-console.log('' || 'Jonas'); // Jonas
-console.log(true || 0); // true
-console.log(undefined || null); // null
-console.log(undefined || 0 || '' || 'Hello' || 23 || null); // Hello
+// // ------ || ------
+// // Use ANY data type, return ANY data type;
+// console.log(3 || 'Jonas'); // 3
+// console.log('' || 'Jonas'); // Jonas
+// console.log(true || 0); // true
+// console.log(undefined || null); // null
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null); // Hello
 
-restaurant.numGuests = 23;
-let guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1); // 23
-let guests2 = restaurant.numGuests || 10;
-console.log(guests2); // 23
+// restaurant.numGuests = 23;
+// let guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1); // 23
+// let guests2 = restaurant.numGuests || 10;
+// console.log(guests2); // 23
 
-restaurant.numGuests = 0;
-guests2 = restaurant.numGuests || 10;
-console.log(guests2); // 10
+// restaurant.numGuests = 0;
+// guests2 = restaurant.numGuests || 10;
+// console.log(guests2); // 10
 
-// ------ && ------
-console.log('------ && ------');
-console.log(0 && 'Jonas'); // 0
-console.log(7 && 'Jonas'); // Jonas (it returns the latest value)
+// // ------ && ------
+// console.log('------ && ------');
+// console.log(0 && 'Jonas'); // 0
+// console.log(7 && 'Jonas'); // Jonas (it returns the latest value)
 
-console.log('Hello' && 23 && null && 'Jonas'); // null
+// console.log('Hello' && 23 && null && 'Jonas'); // null
 
-// Practical Example
-if (restaurant.orderPizza) {
-  restaurant.orderPizza('mushrooms', 'spinac'); // mushrooms ['spinac']
-}
+// // Practical Example
+// if (restaurant.orderPizza) {
+//   restaurant.orderPizza('mushrooms', 'spinac'); // mushrooms ['spinac']
+// }
 
-restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinac'); // mushrooms ['spinac']
+// restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinac'); // mushrooms ['spinac']
 
-// ----- The Nullish Coalescing Operator (??) -----
-restaurant.numGuests = 0;
-const guests3 = restaurant.numGuests || 10;
-console.log(guests3); // 10
+// // ----- The Nullish Coalescing Operator (??) -----
+// restaurant.numGuests = 0;
+// const guests3 = restaurant.numGuests || 10;
+// console.log(guests3); // 10
 
-// Nullish: null and undefined (NOT 0 or '')
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect); // 0
+// // Nullish: null and undefined (NOT 0 or '')
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect); // 0
 
-// ----- Logical Assignment Operators -----
-const rest1 = { name: 'Capri', numGuests: 0 };
-const rest2 = { name: 'La Pizza', owner: 'Giovanni Roiss' };
+// // ----- Logical Assignment Operators -----
+// const rest1 = { name: 'Capri', numGuests: 0 };
+// const rest2 = { name: 'La Pizza', owner: 'Giovanni Roiss' };
 
-// rest1.numGuests = rest1.numGuests || 10;
-// rest2.numGuests = rest2.numGuests || 10;
+// // rest1.numGuests = rest1.numGuests || 10;
+// // rest2.numGuests = rest2.numGuests || 10;
 
-// Or assigment operator
-// rest1.numGuests ||= 10; 
-// rest2.numGuests ||= 10; 
+// // Or assigment operator
+// // rest1.numGuests ||= 10;
+// // rest2.numGuests ||= 10;
 
-// console.log(rest1); // {name: 'Capri', numGuests: 10}
-// console.log(rest2); // {name: 'La Pizza', owner: 'Giovanni Roiss', numGuests: 10}
+// // console.log(rest1); // {name: 'Capri', numGuests: 10}
+// // console.log(rest2); // {name: 'La Pizza', owner: 'Giovanni Roiss', numGuests: 10}
 
+// // nullish assigment operator (null or undefined)
+// // rest1.numGuests ??= 10;
+// // rest2.numGuests ??= 10;
 
-// nullish assigment operator (null or undefined)
-// rest1.numGuests ??= 10; 
-// rest2.numGuests ??= 10;
+// // console.log(rest1); // {name: 'Capri', numGuests: 0}
+// // console.log(rest2); // {name: 'La Pizza', owner: 'Giovanni Roiss', numGuests: 10}
+
+// // AND assigment operator
+// // rest1.owner = rest1.owner && '<ANONYMOUS>'; // {name: 'Capri', numGuests: 0, owner: undefined}
+// // rest2.owner = rest2.owner && '<ANONYMOUS>'; // {name: 'La Pizza', owner: '<ANONYMOUS>', numGuests: 10}
+// rest1.owner &&= '<ANONYMOUS>';
+// rest2.owner &&= '<ANONYMOUS>';
 
 // console.log(rest1); // {name: 'Capri', numGuests: 0}
-// console.log(rest2); // {name: 'La Pizza', owner: 'Giovanni Roiss', numGuests: 10}
+// console.log(rest2); // {name: 'La Pizza', owner: '<ANONYMOUS>'}
 
-// AND assigment operator
-// rest1.owner = rest1.owner && '<ANONYMOUS>'; // {name: 'Capri', numGuests: 0, owner: undefined}
-// rest2.owner = rest2.owner && '<ANONYMOUS>'; // {name: 'La Pizza', owner: '<ANONYMOUS>', numGuests: 10}
-rest1.owner &&= '<ANONYMOUS>';
-rest2.owner &&= '<ANONYMOUS>';
+// ---------------- CHALLENGE ----------------
 
-console.log(rest1); // {name: 'Capri', numGuests: 0}
-console.log(rest2); // {name: 'La Pizza', owner: '<ANONYMOUS>'}
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+
+GOOD LUCK 😀
+*/
+
+// 1. 
+const [players1, players2] = game.players;
+console.log(players1); // ['Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski']
+console.log(players2); // ['Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Witsel', 'Hazard', 'Brandt', 'Sancho', 'Gotze']
+
+// 2.
+const [gk, ...fieldPlayers] = players1;
+console.log(gk); // Neuer (REST Syntax)
+console.log(fieldPlayers); // ['Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski']
+
+// 3.
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers); // ['Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Burki', 'Schulz', 'Hummels', 'Akanji', 'Hakimi', 'Weigl', 'Witsel', 'Hazard', 'Brandt', 'Sancho', 'Gotze']
+
+// 4.
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final); // ['Neuer', 'Pavard', 'Martinez', 'Alaba', 'Davies', 'Kimmich', 'Goretzka', 'Coman', 'Muller', 'Gnarby', 'Lewandowski', 'Thiago', 'Coutinho', 'Perisic']
+
+// 5.
+const {odds: { team1, x: draw, team2} } = game;
+console.log(team1);
+console.log(draw);
+console.log(team2);
+
+// 6.
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7.
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
