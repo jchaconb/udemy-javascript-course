1c24e58b98104dc51d4d3fb7f0ddc849d992b3f4
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-04 15:19:52 -0600

    Section 9.126: Add notes to capture key concepts

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -1,5 +1,45 @@
 'use strict';
 
+console.log('-------- Working With String --------');
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]); // A
+console.log(plane[1]); // 3
+console.log(plane[2]); // 2
+console.log('B737'[0]); // B
+
+console.log(airline.length); // 16
+console.log('B737'.length); // 4
+
+console.log(airline.indexOf('r')); // 6
+console.log(airline.lastIndexOf('r')); // 10
+console.log(airline.indexOf('portugal')); // -1
+
+console.log(airline.slice(4)); // 'Air Portugal'
+console.log(airline.slice(4, 7)); // 'Air'
+
+console.log(airline.slice(0, airline.indexOf(' '))); // 'TAP'
+console.log(airline.slice(airline.lastIndexOf(' ') + 1)); //  'Portugal'
+
+console.log(airline.slice(-2)); // al
+console.log(airline.slice(1, -1)); // AP Air Portuga
+
+const checkMiddleSeat = function (seat) {
+  // B and E are middle seats
+  const s = seat.slice(-1);
+  if (s === 'B' || s === 'E') {
+    console.log('You got the middle seat');
+  } else {
+    console.log('You got lucky!!!');
+  }
+};
+
+checkMiddleSeat('11B');
+checkMiddleSeat('23C');
+checkMiddleSeat('3E');
+
 ///////////////////////////////////////
 // Coding Challenge #3
 
