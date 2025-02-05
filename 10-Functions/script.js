8deb04a6dfd84bb06b7634eb3bc9641173c5ccfb
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-05 15:26:15 -0600

    Section 10.136: Add notes to capture key concepts

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -22,7 +22,7 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 createBooking('LH123'); // {flightNum: 'LH123', numPassengers: 1, price: 199}
 createBooking('LH123', 2, 800); // {flightNum: 'LH123', numPassengers: 2, price: 800}
 createBooking('LH123', undefined, 1000); // {flightNum: 'LH123', numPassengers: 1, price: 1000}
-*/
+
 
 console.log('------ How Passing Arguments Works: Value vs. Reference ------');
 
@@ -54,5 +54,34 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 console.log(jonas);
+*/
 
+console.log('------ Functions Accepting Callback Functions ------');
 
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+console.log('***** Higher-order function *****');
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord); // Transformed string: JAVASCRIPT is the best!
+transformer('JavaScript is the best!', oneWord); // javascriptisthebest!
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
