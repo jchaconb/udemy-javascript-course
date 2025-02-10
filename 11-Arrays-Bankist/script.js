9f9caf6e7bffab35ebd9f3c0eac3a1b4341000f5
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-10 10:32:47 -0600

    Section 11.168: Add notes to capture key concepts

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -298,3 +298,26 @@ console.log('== Separate callback ==');
 const deposit = mov => mov > 0;
 console.log(movements.every(deposit)); // false
 console.log(account4.movements.every(deposit)); // true
+
+console.log('-------- flat and flatMap --------');
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat()); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat()); // [Array(2), 3, 4, Array(2), 7, 8]
+console.log(arrDeep.flat(2)); // [1, 2, 3, 4, 5, 6, 7, 8]
+
+console.log('== flat ==');
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance); // 17840
+
+console.log('== flatMap ==');
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2); // 17840
+
