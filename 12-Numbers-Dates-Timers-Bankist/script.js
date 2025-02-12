88f4e1ec177352bbafb1ae9a7486ba8efc4f0482
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-12 15:21:35 -0600

    Section 12.181: Add notes to capture key concepts

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -344,3 +344,22 @@ console.log((2.7).toFixed(0)); // '3'
 console.log((2.7).toFixed(3)); // '2.700'
 console.log((2.345).toFixed(2)); // '2.35'
 console.log(+(2.345).toFixed(2)); // 2.35
+
+console.log('----- The Remainder Operator -----');
+console.log(5 % 2); // 1
+console.log(5 / 2); // 2.5
+
+console.log(8 % 3); // 2
+console.log(8 % 3); // 2.6666
+
+console.log(6 % 2); // 0
+console.log(6 / 3); // 2
+
+console.log(7 % 2); // 1
+console.log(7 / 2); // 3.5
+
+const isEven = n => n % 2 === 0;
+
+console.log(isEven(8)); // true
+console.log(isEven(23)); // false
+console.log(isEven(514)); // true
