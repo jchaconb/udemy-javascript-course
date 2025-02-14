620776deadd2296f55645e2eeb1e8c87eac5ab30
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-14 11:35:13 -0600

    Section 13.197: Add notes to capture key concepts

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -30,7 +30,6 @@ document.addEventListener('keydown', function (e) {
 });
 
 console.log('----- Selecting, Creating, and Deleting Elements -----');
-
 const header = document.querySelector('.header');
 
 console.log(header);
@@ -59,3 +58,32 @@ document
     // message.remove();
     message.parentElement.removeChild(message);
   });
+
+console.log('----- Styles, Attributes and Classes -----');
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+
+console.log(getComputedStyle(message).color); // rgb(187, 187, 187)
+console.log(getComputedStyle(message).height); // 49px
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 40 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt); // Bankist logo
+console.log(logo.src); // .../13-Advanced-DOM-Bankist/img/logo.png
+console.log(logo.getAttribute('src')); // img/logo.png
+
+logo.alt = 'Beautiful minimalist logo'; //
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href); // .../13-Advanced-DOM-Bankist/index.html#
+
+logo.classList.add('a', 'f');
+logo.classList.remove('b');
+logo.classList.toggle('c');
+logo.classList.contains('d');
