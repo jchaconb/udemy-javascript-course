e65ab570d4869dcae824336bd446d8358c3dfc93
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-14 15:20:59 -0600

    Section 13.201: Add notes to capture key concepts

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -109,10 +109,7 @@ logo.classList.remove('b');
 logo.classList.toggle('c');
 logo.classList.contains('d');
 
-*/
-
 console.log('----- Types of Events Handlers -----');
-
 const h1 = document.querySelector('h1');
 
 const alertH1 = function () {
@@ -126,3 +123,28 @@ const alertH1 = function () {
 // };
 
 h1.addEventListener('mouseenter', alertH1);
+*/
+
+console.log('----- Event Propagation in Practice -----');
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  // e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV', e.target, e.currentTarget);
+});
