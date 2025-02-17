9e106910fceadba9e21d585bfeb203b840f74849
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-17 16:39:03 -0600

    Section 14.220: Add notes to capture key concepts

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -28,3 +28,27 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack); //  Person {firstName: 'Matilda', birthYear: 2017} Person {firstName: 'Jack', birthYear: 1975}
 console.log(jonas instanceof Person); // true
 
+console.log('---------- Prototypes ----------');
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+// .prototyeOfLinkedObjects
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas.species, matilda.species);
+
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
