294708e506fc8b9fa0bf97487115be5f82b0e8a7
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-18 15:33:36 -0600

    Section 14.237: Complete Challenge #4

--- 14-OOP/script.js
+++ 14-OOP/script.js
@@ -519,3 +519,80 @@ console.log(acc1.pin); // undefined
 console.log(acc1.getMovements()); // [250, -140, 1000]
 
 // console.log(acc1.#somePrivateMethod()); // Uncaught SyntaxError: Private field '#somePrivateMethod' must be declared in an enclosing class
+
+console.log('---------- Challenge #4 ----------');
+/* 
+1. Re-create challenge #3, but this time using ES6 classes: create an 'EVCl' child class of the 'CarCl' class
+2. Make the 'charge' property private;
+3. Implement the ability to chain the 'accelerate' and 'chargeBattery' methods of this class, and also update the 'brake' method in the 'CarCl' class. They experiment with chining!
+
+DATA CAR 1: 'Rivian' going at 120 km/h, with a charge of 23%
+*/
+
+class Vehicle {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+
+    return this;
+  }
+
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.make} is going at ${this.speed} km/h`);
+
+    return this;
+  }
+
+  get speedUS() {
+    console.log(`${this.make} is going at ${this.speed / 1.6} mi/h`);
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+class ElectricVehicle extends Vehicle {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge -= 1;
+
+    console.log(
+      `${this.make} going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }%`
+    );
+
+    return this;
+  }
+}
+
+const rivian = new ElectricVehicle('Rivian', 120, 23);
+
+rivian
+  .accelerate()
+  .accelerate()
+  .accelerate()
+  .brake()
+  .chargeBattery(50)
+  .accelerate();
+
+rivian.speedUS;
