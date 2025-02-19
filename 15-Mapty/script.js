34e97d9f787f618c360d22a30dcd14441453d257
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-19 12:36:59 -0600

    Section 15.244: Add functionality to display a map marker

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -15,7 +15,9 @@ if (navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(
     function (position) {
       const { latitude, longitude } = position.coords;
+
       const coords = [latitude, longitude];
+
       const map = L.map('map').setView(coords, 15);
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -23,10 +25,25 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout')
+          .openPopup();
+
+        console.log(mapEvent);
+      });
     },
     function () {
       alert('Could not get your position');
