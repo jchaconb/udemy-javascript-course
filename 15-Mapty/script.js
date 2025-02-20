d6786f94b6c0741a445a47bed8ca1fcbe681087d
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-20 14:17:11 -0600

    Section 15.251: Add functionality to move to market on click

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -69,6 +69,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class App {
   // Private instances
   #map;
+  #mapZoomLevel = 15;
   #mapEvent;
   #workouts = [];
 
@@ -76,6 +77,7 @@ class App {
     this._getPosition();
     form.addEventListener('submit', this._newWorkout.bind(this));
     inputType.addEventListener('change', this._toggleElevationField);
+    containerWorkouts.addEventListener('click', this._moveToPopup.bind(this));
   }
 
   _getPosition() {
@@ -94,7 +96,7 @@ class App {
 
     const coords = [latitude, longitude];
 
-    this.#map = L.map('map').setView(coords, 15);
+    this.#map = L.map('map').setView(coords, this.#mapZoomLevel);
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
@@ -260,9 +262,22 @@ class App {
       `;
     }
 
-    console.log(html);
-
     form.insertAdjacentHTML('afterend', html);
+  }
+
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest('.workout');
+
+    if (!workoutEl) return;
+
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+
+    this.#map.setView(workout.coords, this.#mapZoomLevel, {
+      animate: true,
+      pan: { duration: 1 },
+    });
   }
 }
 
