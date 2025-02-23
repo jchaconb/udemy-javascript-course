ce96633bfbcc3a88e01ff69b24b8592c1c8482eb
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-23 10:44:18 -0600

    Section 16.269: Add notes to capture key concepts

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -1,11 +1,43 @@
 'use strict';
 
+console.log('-- Building a Simple Promise --');
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log('Lotter draw is happenning 🔮');
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve('You WIN 💰');
+    } else {
+      reject(new Error('You lost your money 💩'));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+console.log('Promisifying setTimeout');
+const wait = function(seconds) {
+  return new Promise(function(resolve) {
+    setTimeout(resolve, seconds * 1000);
+  })
+}
+
+wait(2).then(() => {
+  console.log('I waited for 2 seconds');
+  return wait(1)
+}).then(() => {
+  console.log('I waited for 1 second');
+})
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject(new Error('Problem!')).then(x => console.error(x));
+
+/*
 console.log('-- The event loop in practice --');
 console.log('Test start');
 setTimeout(() => console.log('0 sec timer'), 0);
 Promise.resolve('Resolved promise 1').then(res => console.log(res));
 Promise.resolve('Resolved promise 2').then(res => {
-  for (let i = 0; i < 10000000000; i++) {}
+  for (let i = 0; i < 100000000; i++) {}
   console.log(res);
 });
 console.log('Test end');
@@ -15,4 +47,4 @@ console.log('Test end');
 // Resolved promise 1
 // Resolved promise 2
 // 0 sec timer
-
+*/
