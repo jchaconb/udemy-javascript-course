aef582fcc9fcd23804fcf976504bcd2a1ab59bad
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-26 12:52:40 -0600

    Section 17.287: Introduction to NPM

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -34,7 +34,6 @@ console.log(lastPost); // Promise {<pending>}
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
-*/
 
 const ShoppingCart2 = (function () {
   const cart = [];
@@ -64,3 +63,22 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+*/
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
