b6b8428922b950d255ad57fa8775cb4cdd51ac65
Alberto Chacón <josealberto.ch.b@gmail.com>
2025-02-27 12:14:14 -0600

    Section 18.298: Loading a Recipe from API

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -12,3 +12,33 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const recipeURL =
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886';
+    const res = await fetch(recipeURL);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${data.status})`);
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.sourceUrl,
+      image: recipe.image,
+      servings: recipe.servings,
+      cookingTime: recipe.cookingTime,
+      ingredients: recipe.ingredients,
+    };
+
+    console.log(recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+showRecipe();
